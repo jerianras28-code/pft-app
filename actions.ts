223921fb7dd50a742f65c910ec8c6b8d88aec5d6
Jerian Ras <jerianras28@gmail.com>
2026-03-10 00:19:41 -0400

    bug fix* -added get user session into a function for proper await/async syntax

--- actions.ts
+++ actions.ts
@@ -5,12 +5,17 @@ import { db } from "./db";
 import { transactionTable } from "./db/schema";
 import { eq, and } from "drizzle-orm";
 
-// Getting user session id
-const session = await auth();
-const userId = session?.user?.id ?? redirect("/api/auth/signin");
+async function getUserId() {
+  // Getting user session id
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!userId) redirect("/api/auth/signin");
+  return userId;
+}
 
 export async function transactionData() {
   // getting transactions
+  const userId = await getUserId();
   const transactions = await db
     .select()
     .from(transactionTable)
@@ -19,6 +24,7 @@ export async function transactionData() {
 }
 
 export async function deleteTransaction(id: number) {
+  const userId = await getUserId();
   const transactions = await db
     .delete(transactionTable)
     .where(
